fab650258615e894fbd7e6095d60dcb1ef2e37a1
ida-work <i21155388@gmail.com>
2025-11-20 17:12:51 +0900

    rv telescope class

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,8 +4,8 @@ $providers = [
     App\Providers\AppServiceProvider::class,
 ];
 
-// Telescopeはローカル環境でのみ有効化
-if (app()->environment('local')) {
+// Telescopeはローカル環境でのみ有効化（クラスが存在する場合のみ）
+if (app()->environment('local') && class_exists(\Laravel\Telescope\TelescopeApplicationServiceProvider::class)) {
     $providers[] = App\Providers\TelescopeServiceProvider::class;
 }
 
